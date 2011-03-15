b0af189682f47abbb897c7efc5d10c1b48bfb27c
Hajime Hoshi <hajime@hajime-VirtualBox.(none)>
2011-03-16 03:30:34 +0900

    first commit

---
+++ hitozuma.rb
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+require 'net/irc'
+require 'config.rb'
+
+class NBot < Net::IRC::Client
+
+  def on_rpl_welcome(m)
+    OPTIONS['channels'].each do |channel|
+      post(JOIN, channel)
+    end
+  end
+
+  def on_privmsg(m)
+    super
+    channel, message = *m
+    m.prefix =~ /^(.+?)\!/
+    nick = $1
+    now = Time.now
+    ch = channel.sub(/^\#/, '')
+    message.force_encoding("UTF-8")
+    if rand(100) == 0
+      answer = (rand(10) != 0) ? "はい" : "いいえ"
+      if nick
+        message = "#{nick}: #{answer}"
+      else
+        message = answer
+      end
+      post(NOTICE, channel, message)
+    end
+  end
+
+end
+
+Process.daemon(true)
+
+NBot.new(OPTIONS['host'], OPTIONS['port'],
+         nick: OPTIONS['nick'],
+         user: OPTIONS['user'],
+         real: OPTIONS['real']).start
